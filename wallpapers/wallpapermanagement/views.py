662ae8d5f483559427f7d48f6cc82fc7bd3b817b
ManthanSatani <satanimanthan@gmail.com>
2018-10-15 16:10:21 +0530

    updated wallpapers and inserted all categories into the data base

--- wallpapers/wallpapermanagement/views.py
+++ wallpapers/wallpapermanagement/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Categories
 
 def home(request):
     return render(request, 'user/category.html', {'items': [1, 2, 3, 4, 5, 6]})
 
 
 def categories(request):
-    return render(request, 'user/category.html', {'items': [1, 2, 3, 4, 5, 6]})
+    categories = Categories.objects
+    return render(request, 'user/category.html', {'categories': categories})
